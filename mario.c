d7a668a45d24ffc2b4db0e1d2ab0a917f42eac29
Miracle656 <83222244+Miracle656@users.noreply.github.com>
2023-11-20 04:55:11 +0000

    update

--- mario.c
+++ mario.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World\n");
+    int n;
+    do{
+        printf("Height: ");
+        scanf("%d", &n);
+    }while(n <= 0);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
+            printf("#");
+        }
+        printf("\n");
+    }
 }
